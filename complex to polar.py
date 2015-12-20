8df6fb35753e08882458444fefa28eb65629572d
vsham20 <vsham20@gmail.com>
2015-12-20 11:00:14 +0530

    New

---
+++ complex to polar.py
@@ -0,0 +1,6 @@
+__author__ = 'vaishali'
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from cmath import *
+a = polar(complex(raw_input())) # polar() converts complex number into polar
+print a[0]
+print a[1]
