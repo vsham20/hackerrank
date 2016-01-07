ac91bde0d0e063f3bd5d273b159da5e645c570a4
vsham20 <vsham20@gmail.com>
2016-01-07 11:42:45 +0530

    new

---
+++ dot_product.py
@@ -0,0 +1,6 @@
+__author__ = 'vaishali'
+import numpy
+N = int(raw_input())
+A =  numpy.array([map(int, raw_input().split()) for i in range(N)])
+B =  numpy.array([map(int, raw_input().split()) for i in range(N)])
+print numpy.dot(A, B)
